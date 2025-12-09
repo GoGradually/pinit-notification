f7560779930053b0b3148905ebadfffd0998de8d
GoGradually <chkun3109@gmail.com>
2025-12-09 17:03:51 +0900

    initial commit

---
+++ src/main/java/me/pinitnotification/PinitNotificationApplication.java
@@ -0,0 +1,13 @@
+package me.pinitnotification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinitNotificationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PinitNotificationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/pinitnotification/PinitNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package me.pinitnotification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PinitNotificationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
