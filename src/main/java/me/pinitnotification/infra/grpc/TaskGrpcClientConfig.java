58b05aa0fef7a1e6cec1508c962d9b1ab7e4aa12
GoGradually <chkun3109@gmail.com>
2025-12-12 21:20:36 +0900

    feat: TaskGrpcClientConfig 클래스 추가 및 gRPC 클라이언트 설정

---
+++ src/main/java/me/pinitnotification/infra/grpc/TaskGrpcClientConfig.java
@@ -0,0 +1,25 @@
+package me.pinitnotification.infra.grpc;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import me.gg.pinit.pinittask.grpc.ScheduleGrpcServiceGrpc;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties(TaskGrpcProperties.class)
+public class TaskGrpcClientConfig {
+
+    @Bean(destroyMethod = "shutdown")
+    public ManagedChannel scheduleManagedChannel(TaskGrpcProperties properties) {
+        return ManagedChannelBuilder.forAddress(properties.host(), properties.port())
+                .usePlaintext()
+                .build();
+    }
+
+    @Bean
+    public ScheduleGrpcServiceGrpc.ScheduleGrpcServiceBlockingStub scheduleGrpcServiceBlockingStub(ManagedChannel channel) {
+        return ScheduleGrpcServiceGrpc.newBlockingStub(channel);
+    }
+}
