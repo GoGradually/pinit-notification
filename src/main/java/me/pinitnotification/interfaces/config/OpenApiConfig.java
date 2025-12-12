4ef92faf518c1531063430cce87f12364352b1bb
GoGradually <chkun3109@gmail.com>
2025-12-12 21:47:33 +0900

    feat: OpenAPI 설정 추가 및 springdoc-openapi 의존성 등록

---
+++ src/main/java/me/pinitnotification/interfaces/config/OpenApiConfig.java
@@ -0,0 +1,22 @@
+package me.pinitnotification.interfaces.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        Server production = new Server()
+                .url("https://notification.pinit.go-gradually.me")
+                .description("Production");
+
+        return new OpenAPI()
+                .servers(List.of(production));
+    }
+}
