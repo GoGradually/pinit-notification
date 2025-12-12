21d3da5ef6f58c3fef0e8d76b1e8545d1bca46bb
GoGradually <chkun3109@gmail.com>
2025-12-12 20:24:29 +0900

    feat: UpcomingScheduleNotification 엔티티 추가 및 필드 수정

--- src/main/java/me/pinitnotification/domain/notification/UpcomingScheduleNotification.java
+++ src/main/java/me/pinitnotification/domain/notification/UpcomingScheduleNotification.java
@@ -1,16 +1,25 @@
 package me.pinitnotification.domain.notification;
 
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Getter;
 
 import java.util.Map;
 
 @Getter
+@Entity
 public class UpcomingScheduleNotification implements Notification {
-    private final Long scheduleId;
-    private final String scheduleTitle;
-    private final String scheduleStartTime;
-    private final String idempotentKey;
+    @Id
+    @GeneratedValue
+    private Long id;
+    private Long scheduleId;
+    private String scheduleTitle;
+    private String scheduleStartTime;
+    private String idempotentKey;
+
+    protected UpcomingScheduleNotification() {}
 
     public UpcomingScheduleNotification(Long scheduleId, String scheduleTitle, String scheduleStartTime, String idempotentKey) {
         this.scheduleId = scheduleId;
