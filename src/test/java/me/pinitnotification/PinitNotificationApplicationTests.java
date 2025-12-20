11135c1761889da9bb856abd1d0444949fcc9518
GoGradually <chkun3109@gmail.com>
2025-12-20 20:35:56 +0900

    test: PinitNotificationApplicationTests에 FirebaseMessaging 및 JwtTokenProvider 모의 객체 추가

--- src/test/java/me/pinitnotification/PinitNotificationApplicationTests.java
+++ src/test/java/me/pinitnotification/PinitNotificationApplicationTests.java
@@ -2,12 +2,8 @@ package me.pinitnotification;
 
 import com.google.firebase.messaging.FirebaseMessaging;
 import me.pinitnotification.infrastructure.authenticate.JwtTokenProvider;
-import me.pinitnotification.utils.TestKeys;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
@@ -16,20 +12,13 @@ import org.springframework.test.context.bean.override.mockito.MockitoBean;
 @SpringBootTest
 class PinitNotificationApplicationTests {
 
+    @MockitoBean
+    FirebaseMessaging firebaseMessaging;
+    @MockitoBean
+    JwtTokenProvider jwtTokenProvider;
     @Test
     void contextLoads() {
     }
 
-    @TestConfiguration
-    static class TestBeans {
 
-        @MockitoBean
-        FirebaseMessaging firebaseMessaging;
-
-        @Bean
-        @Primary
-        JwtTokenProvider testJwtTokenProvider() {
-            return new JwtTokenProvider(TestKeys.publicKey());
-        }
-    }
 }
